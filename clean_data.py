89aefb267ebf598d8ba35b651f1bf87bd0b80949
sfxpok <ricardo@airmail.cc>
2019-05-12 21:14:14 +0100

    added a tiny bit of code regarding the filters of multiple participations

--- clean_data.py
+++ clean_data.py
@@ -2,3 +2,7 @@ def dropStartCPTimes(df):
     df = df.drop(df[df.checkpoint_order == 0].index)
 
     return df
+
+def createParticipation(filteredAthleteIDs, df): # only meant for multiplePlotting()
+
+    participationsArray = []
